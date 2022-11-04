d6be86dcad6146df8b069ca7ebff9640df82ace1
Rebecca Smith <rebecca.smith1220.rs@gmail.com>
2022-11-03 18:22:03 -0600

    first

---
+++ shaders.py
@@ -0,0 +1,43 @@
+vertex_shader ='''
+#version 450 core
+
+layout (location = 0) in vec3 position;
+layout (location = 1) in vec2 texcoords;
+layout (location = 2) in vec3 normals;
+
+uniform mat4 modelMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
+
+uniform float time;
+out vec2 UVs;
+out vec3 norms;
+out vec3 pos;
+
+void main()
+{
+    UVs = texcoords;
+    norms = normals;
+    pos = (modelMatrix * vec4(position + normals * sin(time * 3)/10, 1.0)).xyz;
+    gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position + normals * sin(time * 3)/10, 1.0);
+
+}
+'''
+
+fragment_shader ='''
+#version 450 core
+
+out vec4 fragColor;
+
+in vec2 UVs;
+in vec3 norms;
+in vec3 pos;
+uniform vec3 pointLight;
+uniform sampler2D tex;
+
+void main()
+{
+    float intensity = dot(norms, normalize(pointLight - pos));
+    fragColor = texture(tex, UVs) * intensity;
+}
+'''
